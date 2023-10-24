28b6354a56eda68ac083ee9286ea474f524bcc24
bpinsard <basile.pinsard@gmail.com>
2023-10-24 14:46:37 -0400

    [DATALAD] Recorded changes

--- code/smriprep_sub-04.sh
+++ code/smriprep_sub-04.sh
@@ -18,7 +18,7 @@ set -e -u -x
 
 export LOCAL_DATASET=$SLURM_TMPDIR/${SLURM_JOB_NAME//-/}/
 export SINGULARITYENV_TEMPLATEFLOW_HOME="${LOCAL_DATASET}/sourcedata/templateflow/"
-flock --verbose /lustre03/project/rrg-pbellec/ria-beluga/alias/cneuromod.anat.smriprep.longitudinal/.datalad_lock datalad clone ria+file:///lustre03/project/rrg-pbellec/ria-beluga#~cneuromod.anat.smriprep.longitudinal@dev $LOCAL_DATASET
+flock --verbose /lustre03/project/rrg-pbellec/ria-beluga/alias/cneuromod.anat.smriprep.longitudinal/.datalad_lock datalad clone ria+file:///lustre03/project/rrg-pbellec/ria-beluga#~cneuromod.anat.smriprep.longitudinal $LOCAL_DATASET
 cd $LOCAL_DATASET
 datalad get -s ria-beluga-storage -J 4 -n -r -R1 . # get sourcedata/* containers
 datalad get -s ria-beluga-storage -J 4 -r sourcedata/templateflow/tpl-{MNI152NLin2009cAsym,OASIS30ANTs,fsLR,fsaverage,MNI152NLin6Asym}
@@ -31,9 +31,9 @@ if [ -d sourcedata/freesurfer ] ; then
   git -C sourcedata/freesurfer checkout -b $SLURM_JOB_NAME
 fi
 
-git submodule foreach  --recursive git-annex enableremote ria-beluga-storage
+git submodule foreach  --recursive bash -c "git-annex enableremote ria-beluga-storage | true"
 
-datalad containers-run -m 'fMRIPrep_sub-04/ses-*' -n bids-fmriprep --input sourcedata/templateflow/tpl-MNI152NLin2009cAsym/ --input sourcedata/templateflow/tpl-OASIS30ANTs/ --input sourcedata/templateflow/tpl-fsLR/ --input sourcedata/templateflow/tpl-fsaverage/ --input sourcedata/templateflow/tpl-MNI152NLin6Asym/ --output . --input 'sourcedata/cneuromod.anat.raw/sub-04/ses-*/anat/*_T1w.nii.gz' --input 'sourcedata/cneuromod.anat.raw/sub-04/ses-*/anat/*_T2w.nii.gz' --input 'sourcedata/cneuromod.anat.raw/sub-04/ses-*/anat/*_FLAIR.nii.gz' --input sourcedata/cneuromod.anat.freesurfer_longitudinal/sub-04 -- -w ./workdir --participant-label 04 --anat-only --bids-filter-file code/bids_filters.json --output-layout bids --output-spaces MNI152NLin2009cAsym T1w:res-iso2mm MNI152NLin6Asym --cifti-output 91k --skip_bids_validation --write-graph --omp-nthreads 8 --nprocs 8 --mem_mb 32768 --fs-license-file code/freesurfer.license  --fs-subjects-dir sourcedata/cneuromod.anat.freesurfer_longitudinal sourcedata/cneuromod.anat.raw ./ participant 
+datalad containers-run -m 'fMRIPrep_sub-04/ses-*' -n bids-fmriprep --input sourcedata/templateflow/tpl-MNI152NLin2009cAsym/ --input sourcedata/templateflow/tpl-OASIS30ANTs/ --input sourcedata/templateflow/tpl-fsLR/ --input sourcedata/templateflow/tpl-fsaverage/ --input sourcedata/templateflow/tpl-MNI152NLin6Asym/ --output . --input 'sourcedata/cneuromod.anat.gradcorrect/sub-04/ses-*/anat/*_T1w.nii.gz' --input 'sourcedata/cneuromod.anat.gradcorrect/sub-04/ses-*/anat/*_T2w.nii.gz' --input 'sourcedata/cneuromod.anat.gradcorrect/sub-04/ses-*/anat/*_FLAIR.nii.gz' --input sourcedata/cneuromod.anat.freesurfer_longitudinal/sub-04 -- -w ./workdir --participant-label 04 --anat-only --bids-filter-file code/bids_filters.json --output-layout bids --output-spaces MNI152NLin2009cAsym T1w:res-iso2mm MNI152NLin6Asym --cifti-output 91k --skip_bids_validation --write-graph --omp-nthreads 8 --nprocs 8 --mem_mb 32768 --fs-license-file code/freesurfer.license  --fs-subjects-dir sourcedata/cneuromod.anat.freesurfer_longitudinal --fs-reuse-base sourcedata/cneuromod.anat.gradcorrect ./ participant 
 fmriprep_exitcode=$?
 
 flock --verbose /lustre03/project/rrg-pbellec/ria-beluga/alias/cneuromod.anat.smriprep.longitudinal/.datalad_lock datalad push -d ./ --to origin
